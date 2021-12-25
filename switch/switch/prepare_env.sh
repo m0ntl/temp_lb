d72378f186b2687d187c60eec972ea056ec52b7b
Adam Montlake <adammontlake@microsoft.com>
2021-12-25 22:12:18 +0200

    add

---
+++ switch/switch/prepare_env.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [[ $(awk -F= '/^NAME/{print $2}' /etc/os-release) != "\"Ubuntu\"" ]] && [[ $(awk -F= '/^VERSION_ID/{print $2}' /etc/os-release) != "\"20.04\"" ]]; 
+then
+	{ echo "Script works only on Ubuntu Focal"; exit $ERRCODE; }
+else
+	{ echo "Starting installation"; }
+fi
+
+cd ..
+echo $PWD
+
+sudo apt update #&& sudo apt upgrade -y
+sudo apt install python-is-python3 python3-pip -y
+
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+source $HOME/.poetry/env
+poetry install
+
+cd switch
+pip install azure.mgmt.compute azure.identity azure.mgmt.network azure.mgmt.storage azure.mgmt.resource azure.monitor.query iohttp
+
+
+
+
+
--- switch/switch/script.sh
+++ switch/switch/script.sh
@@ -1,32 +1,56 @@
 #!/bin/bash
 
-echo "starting script"
-py="python main.py"
+### Update resource names here ###
+sub_id="64fd3ba5-b109-434e-aa61-6dd72c2341f2"
+vmss1_name="ss1-vmss"
+vmss2_name="ss2-vmss"
+rg1_name="vmss1rg"
+rg2_name="vmss2rg"
+lb_name="my-lb"
+hp_name="http_probe"
+bap_name="myvnet-beap"
+lb_rg="myrg"
 
-sub_path="/subscriptions/64fd3ba5-b109-434e-aa61-6dd72c2341f2/"
-vmss1_id="${sub_path}resourceGroups/vmss1rg/providers/Microsoft.Compute/virtualMachineScaleSets/ss1-vmss"
-vmss2_id="${sub_path}resourceGroups/vmss2rg/providers/Microsoft.Compute/virtualMachineScaleSets/ss2-vmss"
-hp_id="${sub_path}resourceGroups/myrg/providers/Microsoft.Network/loadBalancers/my-lb/probes/http_probe"
-bap_id="${sub_path}resourceGroups/myrg/providers/Microsoft.Network/loadBalancers/my-lb/backendAddressPools/myvnet-beap"
+# Resource path definition
+py="python main.py"
+sub_base_path="/subscriptions/${sub_id}"
+vmss1_id="${sub_base_path}/resourceGroups/${rg1_name}/providers/Microsoft.Compute/virtualMachineScaleSets/${vmss1_name}"
+vmss2_id="${sub_base_path}/resourceGroups/${rg2_name}/providers/Microsoft.Compute/virtualMachineScaleSets/${vmss2_name}"
+hp_id="${sub_base_path}/resourceGroups /${lb_rg}/providers/Microsoft.Network/loadBalancers/${lb_name}/probes/${hp_name}"
+bap_id="${sub_base_path}/resourceGroups/${lb_rg}/providers/Microsoft.Network/loadBalancers/${lb_name}/backendAddressPools/${bap_name}"
+lb_ip="10.0.1.4"
+
+# Start of script
+echo "starting script"
 
 #Disconnect both vmss from bap in case they were connected before
-
 {
 	start=`date +%s`
-	$py deregister-vmss --vmss-id $vmss1_id && echo "Deregistration successful for vmss1"
+	$py deregister-vmss --vmss-id $vmss1_id && echo "Deregistration successful for vmss 1"
 	end=`date +%s.%N`
 	runtime1=$( echo "$end - $start" | bc -l )
 } || {
-	echo "Failed to deregister vmss1"
+	echo "Failed to deregister vmss 1"
 }
-
 {
 	start=`date +%s`
-	$py deregister-vmss --vmss-id $vmss2_id && echo "Deregistration successful for vmss2"  
+	$py deregister-vmss --vmss-id $vmss2_id && echo "Deregistration successful for vmss 2"  
 	end=`date +%s.%N`
 	runtime2=$( echo "$end - $start" | bc -l )
 } || {
-	echo "Failed to deregister vmss2"
+	echo "Failed to deregister vmss 2"
 }
 echo "Runtime of deregistering vmss1 from bap: ${runtime1}"
 echo "Runtime of deregistering vmss2 from bap: ${runtime2}"
+
+### test order ###
+# 1. Connect VMSS1
+# 2. Test connection
+# 3. Connect VMSS2
+# 4. Block VMSS1
+# 5. Disconnect VMSS1
+
+$py register-vmss --bap-id $bap_id --vmss-id $vmss1_id --health-probe-id $hp_id
+$py open-vmss --vmss-id $vmss1_id
+#Test connection here
+
