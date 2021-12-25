20197463c2435f1153fdc473b154be3df29787b3
montlake.a <adammontlake@microsoft.com>
2021-12-25 22:40:32 +0200

    add connection of first vmss

--- switch/switch/script.sh
+++ switch/switch/script.sh
@@ -17,9 +17,11 @@ py="python main.py"
 sub_base_path="/subscriptions/${sub_id}"
 vmss1_id="${sub_base_path}/resourceGroups/${rg1_name}/providers/Microsoft.Compute/virtualMachineScaleSets/${vmss1_name}"
 vmss2_id="${sub_base_path}/resourceGroups/${rg2_name}/providers/Microsoft.Compute/virtualMachineScaleSets/${vmss2_name}"
-hp_id="${sub_base_path}/resourceGroups /${lb_rg}/providers/Microsoft.Network/loadBalancers/${lb_name}/probes/${hp_name}"
+hp_id="${sub_base_path}/resourceGroups/${lb_rg}/providers/Microsoft.Network/loadBalancers/${lb_name}/probes/${hp_name}"
 bap_id="${sub_base_path}/resourceGroups/${lb_rg}/providers/Microsoft.Network/loadBalancers/${lb_name}/backendAddressPools/${bap_name}"
 lb_ip="10.0.1.4"
+vmss1_ip="10.0.100.5"
+vmss2_ip="10.0.101.4"
 
 # Start of script
 echo "starting script"
@@ -28,6 +30,7 @@ echo "starting script"
 {
 	start=`date +%s`
 	$py deregister-vmss --vmss-id $vmss1_id && echo "Deregistration successful for vmss 1"
+	$py perform-upgrade --vmss-id $vmss1_id
 	end=`date +%s.%N`
 	runtime1=$( echo "$end - $start" | bc -l )
 } || {
@@ -36,6 +39,7 @@ echo "starting script"
 {
 	start=`date +%s`
 	$py deregister-vmss --vmss-id $vmss2_id && echo "Deregistration successful for vmss 2"  
+	$py perform-upgrade --vmss-id $vmss2_id
 	end=`date +%s.%N`
 	runtime2=$( echo "$end - $start" | bc -l )
 } || {
@@ -51,7 +55,30 @@ echo "Runtime of deregistering vmss2 from bap: ${runtime2}"
 # 4. Block VMSS1
 # 5. Disconnect VMSS1
 
+echo $bap_id
+echo $vmss1_id
+echo $hp_id
+
 $py register-vmss --bap-id $bap_id --vmss-id $vmss1_id --health-probe-id $hp_id
 $py open-vmss --vmss-id $vmss1_id
+$py perform-upgrade --vmss-id $vmss1_id
 #Test connection here
+vmss1_response=$(curl http://$vmss1_ip)
+lb_response=$(curl http://$lb_ip --connect-timeout 3)
+while [ vmss1_response != lb_response]
+do
+	sleep 0.05
+	lb_response=$(curl http://$lb_ip --connect-timeout 3)
+done
+
+
+
+
+
+
+
+
+
+
+
 
