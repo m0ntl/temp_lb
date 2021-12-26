8791fdd9aca0d2ae6f254bb117cdb079c1c0bb2a
montlake.a <adammontlake@microsoft.com>
2021-12-26 10:32:54 +0200

    add

--- switch/switch/script.sh
+++ switch/switch/script.sh
@@ -60,47 +60,58 @@ echo "starting script"
 # 3. Connect VMSS2
 # 4. Block VMSS1
 # 5. Disconnect VMSS1
-echo "\n_____________\nStarting Test"
+echo "Start test"
 $py close-vmss --vmss-id $vmss1_id #for testing only
+sleep 30
 $py register-vmss --bap-id $bap_id --vmss-id $vmss1_id --health-probe-id $hp_id
 $py perform-upgrade --vmss-id $vmss1_id
-#Test connection here
-vmss1_response=$(curl http://$vmss1_ip)
-lb_response=$(curl http://$lb_ip --connect-timeout 3)
-echo "Connected VMSS1 with nsg blocking HP"
-echo "vmss1_response: ${vmss1_response}"
-echo "lb_response: ${lb_response}"
-echo "now opening the NSG and waiting for LB to return correct page..."
-$py open-vmss --vmss-id $vmss1_id
-echo "waiting for LB to reflect vmss1 response"
-while [ "${vmss1_response}" != "${lb_response}" ]
+lb_response=$(curl http://$lb_ip --connect-timeout 3 -s)
+vmss1_response=$(curl http://$vmss1_ip -s)
+while [[ $lb_response != *"Blue"* ]] && [[ $vmss1_response != *"Blue"* ]] then
 do
-	#echo "waiting for LB to reflect vmss1 response"
-	echo -ne "\b$i"
-	sleep 0.05
+	echo "lb rsponse is: ${lb_response}"
+	echo "vmss1 response is: ${vmss1_response}"
+	sleep 1
 	lb_response=$(curl http://$lb_ip --connect-timeout 3 -s)
+	vmss1_response=$(curl http://$vmss1_ip -s)
 done
 
-#switch over to vmss2
-$py open-vmss --vmss-id $vmss2_id
-$py register-vmss --bap-id $bap_id --vmss-id $vmss2_id --health-probe-id $hp_id
-$py perform-upgrade --vmss-id $vmss2_id
-$py close-vmss --vmss-id $vmss1_id #for testing only
-vmss2_response=$(curl http://$vmss2_ip)
-echo "waiting for LB to reflect vmss2 response"
-while [ "${vmss2_response}" != "${lb_response}" ]
-do
-	#echo "waiting for LB to reflect vmss2 response"
-	echo -ne "\b$i"
-	sleep 0.05
-	lb_response=$(curl http://$lb_ip --connect-timeout 3 -s)
-done
-
-
-echo "script DONE"
-echo $vmss1_response
-echo $vmss2_response
-echo $lb_response
+echo "lb rsponse is: ${lb_response}"
+echo "vmss1 response is: ${vmss1_response}"
+#echo "Connected VMSS1 with nsg blocking HP"
+#echo "vmss1_response: ${vmss1_response}"
+#echo "lb_response: ${lb_response}"
+#echo "now opening the NSG and waiting for LB to return correct page..."
+#$py open-vmss --vmss-id $vmss1_id
+#echo "waiting for LB to reflect vmss1 response"
+#while [ "${vmss1_response}" != "${lb_response}" ]
+#do
+#	#echo "waiting for LB to reflect vmss1 response"
+#	echo -ne "\b$i"
+#	sleep 0.05
+#	lb_response=$(curl http://$lb_ip --connect-timeout 3 -s)
+#done
+#
+##switch over to vmss2
+#$py open-vmss --vmss-id $vmss2_id
+#$py register-vmss --bap-id $bap_id --vmss-id $vmss2_id --health-probe-id $hp_id
+#$py perform-upgrade --vmss-id $vmss2_id
+#$py close-vmss --vmss-id $vmss1_id #for testing only
+#vmss2_response=$(curl http://$vmss2_ip)
+#echo "waiting for LB to reflect vmss2 response"
+#while [ "${vmss2_response}" != "${lb_response}" ]
+#do
+#	#echo "waiting for LB to reflect vmss2 response"
+#	echo -ne "\b$i"
+#	sleep 0.05
+#	lb_response=$(curl http://$lb_ip --connect-timeout 3 -s)
+#done
+#
+#
+#echo "script DONE"
+#echo $vmss1_response
+#echo $vmss2_response
+#echo $lb_response
 
 
 
